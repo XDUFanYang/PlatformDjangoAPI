8c872fe2316d4f0076f04378168c5894b23441dc
没有膝盖 <1209202421@qq.com>
2020-04-20 20:40:16 +0800

    api后台更新

---
+++ backend/version_1_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python                                                                  
+# -*-encoding=utf8 -*-                                                             
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('service/', include('apis.urls')),
+    path('auth/', include('authorization.urls'))
+]
